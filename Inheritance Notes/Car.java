20cfe472cad572fc0a5b5f3c837c8cf6efa19938
Chase <esmeraldarekz99@gmail.com>
2017-02-09 12:46:44 -0800

    Save

---
+++ Inheritance Notes/Car.java
@@ -0,0 +1,45 @@
+
+/**
+ * Abstract class Car - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public class Car implements DriverlessCarRules{
+    boolean seatbealtOn ;
+    boolean engineOn ;
+    public Car(){
+        seatbealtOn = false;
+        engineOn = false; 
+    }
+    public void turnOnEngine(){
+        engineOn = true;
+        System.out.println("You turn the key and the engine turns on.");
+    }
+    public void turnOffEngine(){
+        engineOn = false;
+        System.out.println("You turn the key and the engine turns off.");
+   
+    }
+    public boolean seatBeltOn(){
+        seatbealtOn = true;
+        System.out.println("Seatbelt is put on.");
+        return true;
+    }
+    public void seatbeltOff(){
+        seatbealtOn = false;
+        System.out.println("Seatbelt is off.");
+    }
+    public void turnRight(){
+        System.out.println("You have now made a right turn.");
+    }
+    public void turnLeft(){
+     System.out.println("You have now made left turn.");
+    }
+    public void accelerate(int amt){
+        System.out.println("You have now stepped on the gas pedal at "+ amt + ".");
+    }
+    public void reverse(){
+        System.out.println("You are now backing up");
+     } 
+    }
---
+++ Inheritance Notes/DriverlessCarRules.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of interface DriverlessCarRules here.
+ * 
+ * @author Esmeralda Lopez
+ * @version 02.09.17
+ */
+public interface DriverlessCarRules{
+    //methods
+    void accelerate(int amt);
+    //void hjump(int height);
+    //boolean cupholder();
+    //boolean windows();
+    //void moveForward();
+    void reverse();
+    void turnLeft();//done
+    void turnRight();//done
+    boolean seatBeltOn();//done
+    void turnOnEngine();//done
+    void turnOffEngine();//done
+    
+}
